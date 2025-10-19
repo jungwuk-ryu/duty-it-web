079c67ddb7fec47a3aa6df8528d20904248802d5
vojougae35 <vojougae35@gmail.com>
2025-10-19 05:53:43 +0000

    feat: PrimaryCard에 hover 효과 추가

--- app/components/ui/PrimaryCard.tsx
+++ app/components/ui/PrimaryCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function PrimaryCard({ title, content }: Props) {
     return (
-        <div className="drop-shadow-lg rounded-xl bg-white p-10">
+        <div className="drop-shadow-lg rounded-xl bg-white p-10 hover:scale-103 transition-transform hover:shadow-lg">
             <p className="text-xl font-bold leading-relaxed">{title}</p>
             <p className="text-gray-500">{content}</p>
         </div>
