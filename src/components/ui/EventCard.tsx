88996913627677b709481422fd55b13a3ee1153a
vojougae35 <vojougae35@gmail.com>
2025-10-26 08:29:33 +0000

    feat: 행사 섬네일에 Image 태그 사용

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Event } from '@/src/lib/schemas/event';
 import CategoryTag from './EventTypeTag';
 
@@ -8,30 +9,33 @@ type Props = {
 export default function EventCard({ event }: Props) {
     return (
         <a href={event.uri.toString()} aria-label='행사 바로가기'>
-        <div className="rounded-lg hover:scale-103 transition-transform drop-shadow-lg bg-white p-5 cursor-pointer">
-            <div className="relative aspect-[2/1] w-full content-center overflow-hidden mb-3">
-                <img
-                    src={event.thumbnail ?? "/event-thumbnail-placeholder.svg"}
-                    alt="행사 섬네일"
-                    className='rounded-lg object-cover absolute inset-0 w-full h-full'
-                />
-            </div>
-            <div className="flex-1">
-                <CategoryTag category={event.eventType} />
-                <h4 className='text-xl font-bold mt-3 mb-3 truncate'>{event.title}</h4>
-                <div className="space-y-2 text-sm text-gray-600 dark:text-gray-400">
-                    <p><span className="font-semibold w-16 inline-block">주최</span> {event.host.name}</p>
-                    <p><span className="font-semibold w-16 inline-block">일시</span> {formatDates(event.startAt, event.endAt)}</p>
-                    {!(event.recruitmentStartAt == null && event.recruitmentEndAt == null) &&
-                        <p>
-                            <span className="font-semibold w-16 inline-block">
-                                모집
-                            </span>
-                            {formatDates(event.recruitmentStartAt, event.recruitmentEndAt)}
-                        </p>}
+            <div className="rounded-lg hover:scale-103 transition-transform drop-shadow-lg bg-white p-5 cursor-pointer">
+                <div className="relative aspect-[2/1] w-full content-center overflow-hidden mb-3">
+                    <Image
+                        src={event.thumbnail ?? "/event-thumbnail-placeholder.svg"}
+                        alt="행사 섬네일"
+                        fill
+                        priority
+                        className="rounded-lg object-cover"
+                        sizes="(min-width: 768px) 100vw, 50vw"
+                    />
+                </div>
+                <div className="flex-1">
+                    <CategoryTag category={event.eventType} />
+                    <h4 className='text-xl font-bold mt-3 mb-3 truncate'>{event.title}</h4>
+                    <div className="space-y-2 text-sm text-gray-600 dark:text-gray-400">
+                        <p><span className="font-semibold w-16 inline-block">주최</span> {event.host.name}</p>
+                        <p><span className="font-semibold w-16 inline-block">일시</span> {formatDates(event.startAt, event.endAt)}</p>
+                        {!(event.recruitmentStartAt == null && event.recruitmentEndAt == null) &&
+                            <p>
+                                <span className="font-semibold w-16 inline-block">
+                                    모집
+                                </span>
+                                {formatDates(event.recruitmentStartAt, event.recruitmentEndAt)}
+                            </p>}
+                    </div>
                 </div>
             </div>
-        </div>
         </a>
     );
 }
