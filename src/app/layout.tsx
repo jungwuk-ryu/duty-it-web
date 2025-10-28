b1a920e7b2928b4e49e6d05f3e45e54fcc94725d
vojougae35 <vojougae35@gmail.com>
2025-10-28 11:51:03 +0000

    feat: google analytics 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import { Noto_Sans_KR } from "next/font/google";
 import Footer from "../components/Footer";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
         </main>
         <Footer />
       </body>
+      <GoogleTagManager gtmId={process.env.GA_ID ?? ""} />
     </html>
   );
 }
